f204ff33ea4324271d4e61af7313cb4e63ba2b26
clin <clin@thoughtworks.com>
2021-12-23 22:55:46 +0800

    not using uts namespace yet

---
+++ uts_namespace.c
@@ -0,0 +1,27 @@
+#define _GNU_SOURCE
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <sched.h>
+#include <signal.h>
+#include <unistd.h>
+
+#define STACK_SIZE (1024 * 1024)
+
+static char child_stack[STACK_SIZE];
+
+char *const child_args[] = {"/bin/sh", NULL};
+
+int child_main(void* args) {
+  printf("in child process\n");
+  execv(child_args[0], child_args);
+  return 1;
+};
+
+int main(int argc, char *argv[]) {
+  printf("main process \n");
+  int child_pid=clone(child_main, child_stack+STACK_SIZE, SIGCHLD,NULL);
+  waitpid(child_pid,NULL, 0);
+  printf("end process \n");
+  return 0;
+}
